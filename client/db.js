4342707c3e99dc72f0c3662c7ebd6956b53e8b03
Siddharth Dwivedi <57555115+siddhartha285@users.noreply.github.com>
2024-03-15 17:14:02 +0530

    Update db.js

--- client/db.js
+++ client/db.js
@@ -3,6 +3,6 @@ import mysql2 from "mysql2"
 export const db=mysql2.createConnection({
     host:"localhost",
     user:"root",
-    password:"Siddharth@989",
-    database:"blog"
+    password:"",
+    database:""
 })
